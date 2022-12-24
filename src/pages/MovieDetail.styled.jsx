6f7d10fc42262c2e916395c360f961b1b4c90f28
grynka <ihor.kozhemyakin@gmail.com>
2022-12-24 13:37:13 +0200

    fix detail

---
+++ src/pages/MovieDetail.styled.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Detail = styled.div`
+  max-width: 1180px;
+  display: flex;
+  gap: 16px;
+`
+export const Image = styled.img`
+  max-width: 30%;
+`
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,36 +1,48 @@
 import { useParams } from 'react-router-dom';
 import { loadMovies } from 'service/API';
 import { useState, useEffect } from 'react';
+import { Detail, Image } from './MovieDetail.styled';
 
 export const MovieDetails = () => {
-  const [movieDetails, setMovieDetails] = useState(null);
+  const [movieDetails, setMovieDetails] = useState({
+    backdrop_path: '',
+    genres: [],
+    overview: '',
+    poster_path: '',
+    release_date: '',
+    title: '',
+    vote_average: '',
+  });
   const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w780';
   const { id } = useParams();
 
   useEffect(() => {
-    loadMovies('card', id).then(setMovieDetails)
-  }, [id])
+    console.log(id);
+    loadMovies('card', id).then(setMovieDetails);
+  }, [id]);
 
   const { poster_path, title, release_date, vote_average, overview, genres } =
-  movieDetails;
+    movieDetails;
   const imgUrl = poster_path ? `${IMAGE_BASE_URL}/${poster_path}` : 'noPoster';
   const releaseDate = release_date.slice(0, 4);
   const voteAverage = Math.floor(vote_average * 10);
-  const genresStr = genres.map(genre => genre.name).join(' ');
+  const genresStr = genres.map(genre => genre.name).join(', ');
 
   return (
     <main>
-      <img src={imgUrl} alt="" />
-      <div>
-        <h2>
-          {title}( {releaseDate} )
-        </h2>
-        <p>User score: {voteAverage}</p>
-        <h3>Overwiev</h3>
-        <p>{overview}</p>
-        <h3>Genres</h3>
-        <p>{genresStr}</p>
-      </div>
+      <Detail>
+        <Image src={imgUrl} alt={title} />
+        <div>
+          <h2>
+            {title}( {releaseDate} )
+          </h2>
+          <p>User score: {voteAverage}</p>
+          <h3>Overwiev</h3>
+          <p>{overview}</p>
+          <h3>Genres</h3>
+          <p>{genresStr}</p>
+        </div>
+      </Detail>
     </main>
   );
 };
